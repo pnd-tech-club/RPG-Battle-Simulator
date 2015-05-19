efc9e4b7289e4aa7e9420ae53c336d38ab9dae50
Jon "The Real MC <Compadre@The-Swagshack-Real-MCs-crib.local>
2015-05-19 09:29:00 -0500

    Update: Version 0.0.3a

--- RPG-A1 Dev Mode.cpp
+++ RPG-A1 Dev Mode.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : RPG-A1.cpp
 // Author      : The Real MC
-// Version     : 0.0.2
+// Version     : 0.0.3a
 // Copyright   : None
 // Description : RPG - The Battle Scene
 //============================================================================
@@ -13,50 +13,84 @@ using namespace std;
 void fight ();
 void defend ();
 void run ();
+void pstats ();
+void estats ();
 
 int main()
 {
+	//Player and Enemy Turn Switch
+	//0 = Off
+	//1 = On
+	int pt_switch = 0;
+	int et_switch = 0;
+
 	string command;
-	cout << "1 Thug appears! ";
-	cout << "What will you do?\nFIGHT\nDEFEND\nRUN\nEXIT\n";
-	do {
-		getline (cin,command);
-		cout << command << " \n";
-		int strcmp ( const char * str1, const char * str2 );
-		if (command == "FIGHT")
-		{
-			fight ();
-		}
-		else if (command == "DEFEND")
-		{
-			defend ();
-		}
-		else if (command == "RUN")
-		{
-			run ();
-		}
-		else if (command == "EXIT")
-		{
-			return 0;
-		}
-		else
-		{
-			cout << "Invalid response.\n";
-		}
-	} while (command != "EXIT");
+	cout << "Thug Lord Fronk appears! ";
+	do{
+		do {
+			cout << "What will you do?\nFIGHT\nDEFEND\nRUN\nEXIT\n";
+			getline (cin,command);
+			cout << command << " \n";
+			int strcmp ( const char * str1, const char * str2 );
+			if (command == "FIGHT")
+			{
+				pt_switch = 1;
+				fight ();
+			}
+			else if (command == "DEFEND")
+			{
+				pt_switch = 1;
+				defend ();
+			}
+			else if (command == "RUN")
+			{
+				pt_switch = 1;
+				run ();
+			}
+			else if (command == "EXIT")
+			{
+				return 0;
+			}
+			else
+			{
+				cout << "Invalid response.\n";
+			}
+		} while (command != "EXIT");
+	} while (ehealth > 0);
 }
 
+//Fight function
 void fight ()
 {
 	cout << "UNDER CONSTRUCTION\n";
 }
 
+//Defend function
 void defend ()
 {
 	cout << "UNDER CONSTRUCTION\n";
 }
 
+//Run function
 void run ()
 {
 	cout << "UNDER CONSTRUCTION\n";
+}
+
+//player stats
+void pstats ()
+{
+	int phealth = 9999;
+	int pattack = 255;
+	int pdefense = 255;
+	int pspeed = 75;
+}
+
+//enemy stats
+void estats ()
+{
+	int ehealth = 100000;
+	int eattack = 100;
+	int edefense = 100;
+	int espeed = 45;
 }
